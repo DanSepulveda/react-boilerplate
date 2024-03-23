899ba250a907e9187276aa1ec0e45d96362f5485
Daniel Sepúlveda <dansepdev@gmail.com>
2024-03-23 16:58:56 -0300

    useLoginLS custom hook

---
+++ src/hooks/useLoginLS/index.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { loginLS } from '@redux/user'
+import { AppDispatch } from '@app/store'
+import { LS_TOKEN_NAME } from '@app/constants'
+
+export const useLoginLS = () => {
+    const [loading, setLoading] = useState(true)
+    const dispatch = useDispatch<AppDispatch>()
+
+    const loginByToken = async () => {
+        const lsToken = localStorage.getItem(LS_TOKEN_NAME)
+
+        if (lsToken) {
+            await dispatch(loginLS())
+        }
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        loginByToken()
+    }, [])
+
+    return { loading }
+}

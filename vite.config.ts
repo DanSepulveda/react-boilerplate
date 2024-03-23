4504393c6460a7e42aa4b099bffe7187b8a30ece
Daniel Sepúlveda <dansepdev@gmail.com>
2024-03-23 13:54:06 -0300

    path alias added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@app': path.resolve(__dirname, './src/app'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@hooks': path.resolve(__dirname, './src/hooks'),
+            '@redux': path.resolve(__dirname, './src/redux'),
+            '@utils': path.resolve(__dirname, './src/utils'),
+            '@views': path.resolve(__dirname, './src/views'),
+        },
+    },
 })

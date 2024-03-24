a3dfb88f10e1f9330e7be32b3d6e4f9e341acd43
Daniel Sepúlveda <dansepdev@gmail.com>
2024-03-24 08:30:46 -0300

    fix: returning token

--- src/hooks/useLoginLS/index.tsx
+++ src/hooks/useLoginLS/index.tsx
@@ -1,25 +1,26 @@
 import { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { loginLS } from '@redux/user'
+import { useDispatch, useSelector } from 'react-redux'
+import { loginLS, userState } from '@redux/user'
 import { AppDispatch } from '@app/store'
 import { LS_TOKEN_NAME } from '@app/constants'
 
 export const useLoginLS = () => {
-    const [loading, setLoading] = useState(true)
-    const dispatch = useDispatch<AppDispatch>()
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch<AppDispatch>()
+  const { token } = useSelector(userState)
 
-    const loginByToken = async () => {
-        const lsToken = localStorage.getItem(LS_TOKEN_NAME)
+  const loginByToken = async () => {
+    const lsToken = localStorage.getItem(LS_TOKEN_NAME)
 
-        if (lsToken) {
-            await dispatch(loginLS(lsToken))
-        }
-        setLoading(false)
+    if (lsToken) {
+      await dispatch(loginLS(lsToken))
     }
+    setLoading(false)
+  }
 
-    useEffect(() => {
-        loginByToken()
-    }, [])
+  useEffect(() => {
+    loginByToken()
+  }, [])
 
-    return { loading }
+  return { token, loading }
 }

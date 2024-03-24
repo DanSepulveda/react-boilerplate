49d59997f6bfacedf332cda30b6446b828390ae8
Daniel Sepúlveda <dansepdev@gmail.com>
2024-03-23 23:30:20 -0300

    router structure changed

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,14 +1,28 @@
 import { createBrowserRouter } from 'react-router-dom'
 import HomeView from '@views/Home'
 import LoginView from '@views/Login'
+import { ProtectedRoute } from '@components/ProtectedRoute'
+import App from '../App'
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <HomeView />,
-    },
-    {
-        path: '/login',
-        element: <LoginView />,
+        element: <App />,
+        children: [
+            {
+                path: 'login',
+                element: <LoginView />,
+            },
+            {
+                path: '',
+                element: <ProtectedRoute />,
+                children: [
+                    {
+                        path: '',
+                        element: <HomeView />,
+                    },
+                ],
+            },
+        ],
     },
 ])

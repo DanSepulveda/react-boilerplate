54a567b36f88578b2f97bb880051eb9a28e5438d
Daniel Sepúlveda <dansepdev@gmail.com>
2024-03-23 23:29:15 -0300

    fix: redux path alias

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { userReducer } from '@redux/users'
+import { userReducer } from '@redux/user'
 
 export const store = configureStore({
     reducer: {
@@ -12,3 +12,4 @@ export const store = configureStore({
 })
 
 export type State = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch

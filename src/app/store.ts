6f76b9a5ce9457419ba909bd93b696d923c49502
Daniel Sepúlveda <dansepdev@gmail.com>
2024-03-24 08:33:14 -0300

    prettier styles

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,13 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import { userReducer } from '@redux/user'
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: false,
-        }),
+  reducer: {
+    user: userReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 })
 
 export type State = ReturnType<typeof store.getState>
